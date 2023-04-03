6a368c7e570fe9a9d254f5eecf9a437ec1a73df2
323 <323@323-12>
2023-04-03 14:22:47 +0300

    Added FunctionF

--- Program.cs
+++ Program.cs
@@ -3,6 +3,22 @@ public class Program
 {
     public static void Main()
     {
+        double number = EnterDouble();
 
+        Console.WriteLine($"{FunctionF(number)}");
     }
+    private static double EnterDouble()
+    {
+        double number;
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            bool parse = double.TryParse(input, out number);
+            if (parse)
+                break;
+        }
+        return number;
+    }
+
+    private static double FunctionF(double number) => (6 * Math.Sin(number * 2)) - Math.Cos(number * 2);
 }
